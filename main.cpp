2ab86b35980454fd7b3bc240d394672a67562f35
I'm Pancake <slavik15971597@gmail.com>
2023-05-26 14:59:54 -0400

    update2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <boost/asio.hpp>
 #include <iostream>
+#include <string>
 
-using namespace std;
+//using namespace std;
+//using namespace boost;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Step 1. Assume that the client application has already
+    // obtained the IP-address and the protocol port number.
+    std::string raw_ip_address{"127.0.0.1"};
+    unsigned short port_num{3333};
+
+    // Used to store information about error that happens
+    // while parsing the raw IP-address.
+    std::cout << "Hello World!" << std::endl;
+    boost::system::error_code ec;
+
     return 0;
 }
